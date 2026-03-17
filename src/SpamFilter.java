004556399fe2ab17e82d39e9a4fae82257b1f2bf
Urkibiris05 <ubidaurre001@ikasle.ehu.eus>
2026-03-17 15:16:22 +0100

    Bektorizazio kasu bat + instantzia/datu-sorta azterketa

--- src/SpamFilter.java
+++ src/SpamFilter.java
@@ -1,19 +1,21 @@
+
+
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 public class SpamFilter {
 
-    private static SpamFilter nSpamFilter = null;
-    private Bektorizatu bektorizatu;
-    private SpamFilter() {
-        bektorizatu = new Bektorizatu();
+    private static DataProcessor dataProcessor = new DataProcessor();
+    //SMS2Arff sms2Arff = new SMS2Arff();
+
+    public static void main(String[] args) throws Exception {
+        String rawTrainPath = "data/arff/SMS_SpamCollection.train.arff";
+        DataSource source =  new DataSource(rawTrainPath);
+        Instances rawTrain = source.getDataSet();
+        if (rawTrain.classIndex() == -1) rawTrain.setClassIndex(rawTrain.numAttributes() - 1);
+
+        Instances bekData = dataProcessor.bektorizatu(rawTrain);
+        dataProcessor.instantziakAztertu(bekData, "LEHEN FROGA");
     }
 
-    public static SpamFilter getSpamFilter() {
-        if (nSpamFilter == null) {
-            nSpamFilter = new SpamFilter();
-        }
-        return nSpamFilter;
-    }
-
-    public void froga(){
-        bektorizatu.froga();
-    }
 }
